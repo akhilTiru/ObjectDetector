7538cbaace641906240445045192d01a0c155dbd
akhilTiru <141438629+akhilTiru@users.noreply.github.com>
2023-08-11 09:44:47 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preload() {
+    img = loadImage("dog_cat.jpg");
+}
+
+function setup() {
+    //canvas = createCanvas(600 , 400);
+    //canvas.center();
+}
+
+function draw() {
+    //image(img, 0 , 0 , 600 , 400);
+    //fill("red");
+    //text("dog" , 70 , 60);
+    //stroke("red");
+    //noFill();
+    //rect(50 , 40 , 480 , 350);
+    //fill("red");
+    //text("cat" , 300 , 90);
+    //stroke("red");
+    //noFill();
+    //rect(275 , 70 , 280 , 330);
+}
